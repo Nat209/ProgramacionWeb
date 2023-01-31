cf0c1113df5ecb23d9e5b42bd17fc3160d8b2fc5
Estudiante <uestudiantes@uniempresarial.edu.co>
2023-01-31 12:26:32 -0500

    first commit

---
+++ 5.Formularios/main.js
@@ -0,0 +1,15 @@
+'use strict';
+// Variables o contenedores
+
+var numero= 13;//global
+let numero2=25;// Local
+const iva = 19.1;
+const IVA =34;
+if (true) {
+
+    let numero2= 50;
+    console.log(numero2);
+    
+}
+
+console.log(numero2);
