884ed1ab55b0ab7496c73a80d6e2d736094c6229
Nat209 <116594722+Nat209@users.noreply.github.com>
2023-02-17 12:16:32 -0500

    #

--- ProyectoTienda/dist/app.js
+++ ProyectoTienda/dist/app.js
@@ -1,6 +1,6 @@
 'use strict';
 const buscando = document.querySelector('#buscando');
-const inputBuscar = document.querySelector('input_buscar');
+const inputBuscar = document.querySelector('#input_buscar');
 
 buscando.addEventListener('click',()=>{
     if (inputBuscar.classList.contains('buscarOculto')) {
